f0c68b153860de945cd0ccfa37e1dadb05a07bad
Mustafa Baker <mustafabaker2970@gmail.com>
2023-09-26 11:35:07 +0200

    Created a new class, that checks if the two die are the same.

---
+++ isSame.java
@@ -0,0 +1,14 @@
+class isSame {
+
+    public static int isSame(int Die1, int Die2) {
+        
+        if(Die1 == 1 && Die2 == 1) {
+            System.out.println("You lose all points");
+
+        }else if(Die1 == Die2) {
+            System.out.println("You get another throw");
+            DiceCups.sum(Die1, Die2);
+        }
+        return isSame(Die1, Die2);
+    }    
+}
