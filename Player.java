3a51741ae6aee058e995b2adb221d818f62b5986
Frederik Bastrup <frederik.bastrup@gmail.com>
2023-09-26 10:51:27 +0200

    Made code look better and added a comment to code

--- Player.java
+++ Player.java
@@ -2,8 +2,8 @@ class Player {
     public int number;
     public int points;
 
-public Player(int number, int points){
-    this.number = number;
-    this.points = points;
-}
+    public Player(int number, int points) {
+        this.number = number;
+        this.points = points;
+    }
 }
