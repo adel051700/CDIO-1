e900f6ff25d300049648e6ae27502899b59b5b51
Adel Ahmad <adel051700@gmail.com>
2023-09-26 11:38:58 +0200

    Returns the sum plus the players point

--- Dicecup.java
+++ Dicecup.java
@@ -1,6 +1,7 @@
 class DiceCups {
     public static int sum(int Die1, int Die2) {
         int Sum = Die1 + Die2;
-        return (Sum);
+        int plus = Player.points+Sum;
+        return (plus);
     }
 }
