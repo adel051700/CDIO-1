7ca4c51e8cecd3178e0a3ff8d6efa7c87a84430f
Elias Larsen <elias@takefive.dk>
2023-09-26 15:09:47 +0200

    made dicegame in 1 file because we keep failing at multiple classes

--- Dice.java
+++ Dice.java
@@ -5,7 +5,6 @@ class Dice {
 
     public static int rollDie() {
         return (int)Math.floor(Math.random() * 6 + 1);
-        
     }
     
 }
--- Dicegame.java
+++ Dicegame.java
@@ -1,20 +1,68 @@
 class Dicegame {
-        public static void main(String[] args) {
-            var player1 = new Player(1,0);
-            var player2 = new Player(2,0);
 
-            int die1 = Dice.rollDie();
-            int die2 = Dice.rollDie();
+    public static void main(String[] args) {
+        var player1 = 0;
+        var player2 = 0;
+        boolean lastThrowDouble = false;
+        int playerTurn = 1;
+        while (true)
+        {
+        
+        int die1 = (int) Math.floor(Math.random() * 6 + 1);
+        int die2 = (int) Math.floor(Math.random() * 6 + 1);
+        System.out.println(die1 + " " + die2);
 
-            int playerTurn = 1;
-            int deanos = isSame.isSame(die1,die2,playerTurn);
-            System.out.println(playerTurn);
-            
-            public static void updatePoints(int deltaPoints)
+        
+        int deltaPoints = die1 + die2;
+        if (die1 == die2) {
+            if (lastThrowDouble==true)
             {
-                return;
+                System.out.println("Player "+playerTurn+" wins!");
+                break;
             }
+            lastThrowDouble = true;
+            if (playerTurn == 1)
+            {
+                player1 = player1 + deltaPoints;
+            
+            if (die1 == 1) {
+                player1 = 0;
+            } else {
+                player2 = 0;
+            }
+            } else 
+            {
+                player2 = player2 + deltaPoints;
+            }
+        } else if (playerTurn == 1) {
+            player1 = player1 + deltaPoints;
+            
+        } else {
+            player2 = player2 + deltaPoints;
+            
+        }
+        if (player1>=40 || player2>=40)
+        {
+            System.out.println("Player "+playerTurn+" wins!");
+                break;
+        }
+        if (die1!=die2)
+        {
 
-}
-
+        
+        if (playerTurn==1)
+        {
+            playerTurn = 2;
+            lastThrowDouble = false;
+        } 
+        else 
+        {
+            playerTurn = 1;
+            lastThrowDouble = false;
+        }
+        }
+        System.out.println("player1 : " + player1);
+        System.out.println("player2 : " + player2);
+    }
+    }
 }
