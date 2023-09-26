bbe04959103e13b0b10645011f8a6d7b3ef81e0d
Elias Larsen <elias@takefive.dk>
2023-09-26 12:31:42 +0200

    added void and removed return in dicecup

--- Dicecup.java
+++ Dicecup.java
@@ -1,7 +1,7 @@
 class DiceCups {
-    public static int sum(int Die1, int Die2) {
+    public static void sum(int Die1, int Die2) {
         int Sum = Die1 + Die2;
         int plus = Player.points+Sum;
-        return(1);
+        return;
     }
 }
--- Dicegame.java
+++ Dicegame.java
@@ -1,13 +1,13 @@
 class Dicegame {
         public static void main(String[] args) {
-             var player1 = new Player(1,0);
+            var player1 = new Player(1,0);
             var player2 = new Player(2,0);
 
             int die1 = Dice.rollDie();
             int die2 = Dice.rollDie();
 
             int playerTurn = 1;
-            playerTurn = isSame.isSame(die1,die2,playerTurn);
+            int deanos = isSame.isSame(die1,die2,playerTurn);
             System.out.println(playerTurn);
 
 }
