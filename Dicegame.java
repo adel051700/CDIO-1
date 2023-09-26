8906edf71300f1661584264a8128f0facb06cd04
Adel Ahmad <adel051700@gmail.com>
2023-09-26 16:51:24 +0200

    Co-authored-by: Musti :) <WettyWednesday@users.noreply.github.com> Co-authored-by: Mach69ne <Mach69ne@users.noreply.github.com> Co-authored-by: emilsavino <emilsavino@users.noreply.github.com> Co-authored-by: Mads H. <mads369j@users.noreply.github.com> Co-authored-by: FredBas <FredBas@users.noreply.github.com>

--- Dicegame.java
+++ Dicegame.java
@@ -4,14 +4,58 @@ class Dicegame {
         var player1Pnt = 0;
         var player2Pnt = 0;
         boolean lastThrowDouble = false;
-        int playerTurn = 1;
-        while (true) {
+        boolean playerOneTurn = true;
 
+        while (player1Pnt < 40 && player2Pnt < 40) {
             int die1 = (int) Math.floor(Math.random() * 6 + 1);
             int die2 = (int) Math.floor(Math.random() * 6 + 1);
 
-            System.out.println("player1 : " + player1Pnt + " |" + die1 +" & "+ die2);
-            System.out.println("player2 : " + player2Pnt + " |" + die1 +" & "+ die2);
+            if (die1 + die2 != 2) {
+                if (playerOneTurn) {
+                    player1Pnt += die1 + die2;
+                    System.out
+                            .println("Player 1 rolls a " + die1 + " & a " + die2 + " and now has " + player1Pnt + "\n");
+                    playerOneTurn = false;
+                } else {
+                    player2Pnt += die1 + die2;
+                    System.out
+                            .println("Player 2 rolls a " + die1 + " & a " + die2 + " and now has " + player2Pnt + "\n");
+                    playerOneTurn = true;
+                }
+
+                if (die1 == die2) {
+                    if (lastThrowDouble && die1 == 6) {
+                        if (playerOneTurn) {
+                            System.out.println("Player 1 Wins");
+                        } else if (playerOneTurn == false) {
+                            System.out.println("Player 2 Wins");
+                        }
+
+                    }
+
+                    if (die1 == 6) {
+                        lastThrowDouble = true;
+                    }
+                    if (playerOneTurn) {
+                        playerOneTurn = false;
+                    } else {
+                        playerOneTurn = true;
+                    }
+
+                }
+            } else {
+                if (playerOneTurn) {
+                    System.out.println("Player 1 rolled two 1's and loses all points \n");
+                    player1Pnt = 0;
+                } else {
+                    System.out.println("Player 2 rolled two 1's and loses all points \n");
+                    player2Pnt = 0;
+                }
+            }
         }
+            var playerWin = player1Pnt >= 40 ? "Player 1 Wins" : "Player 2 Wins";
+            System.out.println(playerWin);
+
+        
     }
 }
