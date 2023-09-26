30397c846905690c2bf98ef04ac3bcb4061c74b7
Adel Ahmad <adel051700@gmail.com>
2023-09-26 11:36:32 +0200

    Added static to points and number

--- Player.java
+++ Player.java
@@ -1,8 +1,8 @@
 // player object with number and point attributes
 
 class Player {
-    public int number;
-    public int points;
+    public static int number;
+    public static int points;
 
     public Player(int number, int points) {
         this.number = number;
