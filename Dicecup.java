aaa552968b0e7c8673c1d6fb9a6966abca21f04d
Adel Ahmad <adel051700@gmail.com>
2023-09-26 12:06:43 +0200

    made changes to try something in Dicecup

--- Dicecup.java
+++ Dicecup.java
@@ -2,6 +2,6 @@ class DiceCups {
     public static int sum(int Die1, int Die2) {
         int Sum = Die1 + Die2;
         int plus = Player.points+Sum;
-        return (plus);
+        return(1);
     }
 }
