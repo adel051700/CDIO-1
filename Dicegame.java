9b0c2eee65309dd7e0ca06b220bcab2ad92afd4d
Elias Larsen <elias@takefive.dk>
2023-09-26 11:49:41 +0200

    Playerturn not declared before using fixed

--- Dicegame.java
+++ Dicegame.java
@@ -3,10 +3,11 @@ class Dicegame {
             var player1 = new Player(1,0);
             var player2 = new Player(2,0);
 
-           int die1 = Dice.rollDie();
+            int die1 = Dice.rollDie();
             int die2 = Dice.rollDie();
 
-            int playerTurn = isSame.isSame(die1,die2,playerTurn);
+            int playerTurn = 1;
+            playerTurn = isSame.isSame(die1,die2,playerTurn);
             System.out.println(playerTurn);
 
 }
