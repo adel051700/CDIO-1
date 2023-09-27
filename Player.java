db43628e3d3884bd3fec27d27f31227e21ad15e8
Adel Ahmad <adel051700@gmail.com>
2023-09-27 11:20:04 +0200

    Removed player number and set default points to 0

--- Player.java
+++ Player.java
@@ -1,11 +1,9 @@
 // player object with number and point attributes
 
 class Player {
-    public static int number;
     public static int points;
 
     public Player(int number, int points) {
-        this.number = number;
-        this.points = points;
+        this.points = 0;
     }
 }
